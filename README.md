499b942bb365790e34a102036bdf633c7258e7c4
Gilang R <sinyo101520@gmail.com>
2020-04-26 05:22:46 +0700

    Wow

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>My First Calculator, Built with C# Mono</h1>
+
+Wanna get fast? go to my_first_calculator/my_first_calculator/bin/Debug/my_first_calculator.exe
---
+++ my_first_calculator/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace my_first_calculator
+{
+    public class Add
+    {
+        private int aA, Ab;
+        public Add(int a, string add, int b)
+        {
+            this.aA = a;
+            this.Ab = b;
+        }
+
+        public int 
+    }
+}
